f78655024773f44a6be0ba8d1791030b91d3b756
LeonardoMendezR <2108727@ucc.edu.ar>
2025-06-05 10:29:20 -0300

    cambios

--- Backend/wait-for-mysql.sh
+++ Backend/wait-for-mysql.sh
@@ -1,13 +1,10 @@
 #!/bin/sh
 # wait-for-mysql.sh
 set -e
-
 host="$1"
 shift
-
 until nc -z "$host" 3306; do
   echo "Esperando a que MySQL ($host:3306) esté disponible..."
   sleep 2
 done
-
 exec "$@"
